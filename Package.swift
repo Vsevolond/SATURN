81585b1268c421634d1dc2e7b8d793e441baf4d0
Vsevolod Donchenko <vsevolond@MacBook-Pro-Vsevolod.local>
2026-03-18 15:44:47 +0300

    added argument parser

--- Package.swift
+++ Package.swift
@@ -1,26 +1,27 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "saturn",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "saturn",
-            targets: ["saturn"]
-        ),
+        .library(name: "saturn-lib", targets: ["saturn-lib"]),
+        .executable(name: "saturn", targets: ["saturn"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "saturn"
+        .target(name: "saturn-lib"),
+        
+        .executableTarget(
+            name: "saturn",
+            dependencies: [
+                "saturn-lib",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
         ),
-        .testTarget(
-            name: "saturnTests",
-            dependencies: ["saturn"]
-        ),
+        
+        .testTarget(name: "saturn-tests", dependencies: ["saturn-lib"]),
     ]
 )
---
+++ Sources/saturn-lib/saturn-lib.swift
@@ -0,0 +1,7 @@
+//
+//  saturn-lib.swift
+//  saturn
+//
+//  Created by Vsevolod Donchenko on 18.03.2026.
+//
+
